05b55537cf99235e14af1583b6d518377380a258
Jakub Skoczen <jakub@indexdata.dk>
2014-01-23 11:25:03 +0100

    Script to retrieve many records by ID

---
+++ zoomsh/deichman-many.zsh
@@ -0,0 +1,17 @@
+open z3950.deich.folkebibl.no/data
+set preferredRecordSyntax normarc
+set elementSetName F
+find @attr 1=12 1137692
+show 0 1 txml
+find @attr 1=12 1314065
+show 0 1 txml
+find @attr 1=12 804689
+show 0 1 txml
+find @attr 1=12 732535
+show 0 1 txml
+find @attr 1=12 1391753
+show 0 1 txml
+find @attr 1=12 1148838
+show 0 1 txml
+find @attr 1=12 823184
+show 0 1 txml
